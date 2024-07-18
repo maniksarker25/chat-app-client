8fe41d15877083c9c9c33cd536d0b29ebeddcbfe
Manik <tonysins43@gmail.com>
2024-07-18 17:50:57 +0600

    create reuseable input field

---
+++ src/components/Forms/CInput.tsx
@@ -0,0 +1,46 @@
+import { SxProps, TextField } from "@mui/material";
+import { Controller, useFormContext } from "react-hook-form";
+
+type TInputProps = {
+  name: string;
+  label?: string;
+  type?: string;
+  size?: "small" | "medium";
+  fullWidth?: boolean;
+  required?: boolean;
+  sx?: SxProps;
+};
+
+const CInput = ({
+  name,
+  label,
+  type = "text",
+  size = "medium",
+  fullWidth,
+  required,
+  sx,
+}: TInputProps) => {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          label={label}
+          type={type}
+          required={required}
+          variant="outlined"
+          size={size}
+          fullWidth={fullWidth}
+          error={!!error?.message}
+          helperText={error?.message}
+          sx={{ ...sx }}
+        />
+      )}
+    />
+  );
+};
+
+export default CInput;
