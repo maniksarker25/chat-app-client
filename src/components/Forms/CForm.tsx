b3ab0c9bb80ebfd64956107b2d56d5bbe0455df5
Manik <tonysins43@gmail.com>
2024-07-18 13:20:22 +0600

    crate reuseable form component

---
+++ src/components/Forms/CForm.tsx
@@ -0,0 +1,27 @@
+import {
+  FieldValues,
+  FormProvider,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+
+type TFormProps = {
+  children: React.ReactNode;
+  onSubmit: SubmitHandler<FieldValues>;
+};
+
+const CForm = ({ children, onSubmit }: TFormProps) => {
+  const methods = useForm();
+  const { handleSubmit } = methods;
+  const submit: SubmitHandler<FieldValues> = (data) => {
+    onSubmit(data);
+  };
+
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(submit)}>{children}</form>
+    </FormProvider>
+  );
+};
+
+export default CForm;
