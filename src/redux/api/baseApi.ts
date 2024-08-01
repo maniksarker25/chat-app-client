7504022c2ea96ca68b4a45aa7370829c1ce6e1f4
Manik <tonysins43@gmail.com>
2024-08-01 18:01:12 +0600

    configure redux in project

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,8 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  endpoints: (builder) => ({}),
+});
