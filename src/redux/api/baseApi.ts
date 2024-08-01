e560be4b866027f4b2349d6d861e8126851f4f46
Manik <tonysins43@gmail.com>
2024-08-01 19:57:32 +0600

    create axios base query and make axios instance and interceptor

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,8 +1,9 @@
 // Need to use the React-specific entry point to import createApi
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { axiosBaseQuery } from "@/helpers/axios/axiosBaseQuery";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api" }),
+  baseQuery: axiosBaseQuery({ baseUrl: "http://localhost:5000/api" }),
   endpoints: (builder) => ({}),
 });
