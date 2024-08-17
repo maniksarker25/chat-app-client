383f32aca74bf1d7b5d40426efb264bdbcf9348e
Manik <tonysins43@gmail.com>
2024-08-17 17:07:17 +0600

    create chat layout home page

--- src/app/(chatLayout)/chat/page.tsx
+++ src/app/(chatLayout)/chat/page.tsx
@@ -1,15 +1,29 @@
-import { Box } from "@mui/material";
-
+import { Box, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import chatImage from "../../../../public/chat.png";
 const ChatPage = () => {
   return (
     <Box
       sx={{
-        backgroundColor: "gray",
+        backgroundColor: "#E1E7EF",
         width: "100%",
         height: "100%",
       }}
     >
-      This is chat page
+      <Box>
+        <Stack
+          alignContent={"center"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          justifyItems={"center"}
+          sx={{ height: "100vh" }}
+        >
+          <Image src={chatImage} alt="chatImage" width={100} height={100} />
+          <Typography sx={{ fontSize: "25px", fontWeight: 600 }}>
+            Select user to send message
+          </Typography>
+        </Stack>
+      </Box>
     </Box>
   );
 };
